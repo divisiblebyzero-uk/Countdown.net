4ef183c77479a5d10b85c77b3f16fc9eba463935
Matthew Smalley <dev@divisiblebyzero.co.uk>
2019-12-30 19:01:52 +0000

    Removed test that was taking ages / timing out travis

--- Countdown.net.Test/TestNumbersController.cs
+++ Countdown.net.Test/TestNumbersController.cs
@@ -21,7 +21,6 @@ namespace Countdown.net.Test
         [InlineData("100,3,5,10", 1, 20)]
         [InlineData("100,3,5,100", 1, 0)]
         [InlineData("80,3,5,10", 1, 0)]
-        [InlineData("100,3,5,100,1,2,3,4", 1, 0)]
         public void TestBasicInput(string input, int minimumSolutions, int minClosenessOfBestSolution)
         {
             List<NumbersSolution> solutions = _numbersController.SolveNumbers(input).ToList();
